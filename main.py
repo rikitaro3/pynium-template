b34a34ede9bc92def20b0d6e59c9edc23eb6fb58
rikiya.ohhata <rikiya.ohhata@optim.co.jp>
2023-11-30 12:36:39 +0900

    sample download

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from lib.case import Case
 from lib.e2e_util import Util
-from lib.operation import Click, Get, Screenshot, Submit, Input, SelectBox, Select
+from lib.operation import Click, Get, Screenshot, Submit, Input, SelectBox, Select, DownloadHTML
 
 import my_const as const
 from my_catalog import Catalog
@@ -9,7 +9,8 @@ from my_catalog import Catalog
 def main():
     driver = Util.create_driver(True, False)
     login_case = Case(
-        Get(driver, const.BASE_URL)
+        Get(driver, const.BASE_URL),
+        DownloadHTML(driver, "example.html")
     )
     login_case.exec_operation()
 
