6435f53b9130b528264a875647c14440088fe78e
rikiya.ohhata <rikiya.ohhata@optim.co.jp>
2023-12-04 00:39:21 +0900

    delete sheet

--- lib/spreadsheet_writer.py
+++ lib/spreadsheet_writer.py
@@ -1,11 +1,16 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 
 class SpreadsheetWriter:
     def __init__(self, json_keyfile_name, spreadsheet_name):
-        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-        creds = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile_name, scope)
-        client = gspread.authorize(creds)
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        self.creds = ServiceAccountCredentials.from_json_keyfile_name(
+            json_keyfile_name, scope)
+        client = gspread.authorize(self.creds)
         self.sheet = client.open(spreadsheet_name).sheet1
 
     def write(self, obj_list):
@@ -14,3 +19,14 @@ class SpreadsheetWriter:
         # 各オブジェクトの値を1行のレコードとして出力
         for obj in obj_list:
             self.sheet.append_row(list(obj.values()))
+
+    def delete(self, spreadsheet_name):
+        drive_service = build('drive', 'v3', credentials=self.creds)
+        response = drive_service.files().list(q=f"name='{spreadsheet_name}' and mimeType='application/vnd.google-apps.spreadsheet'",
+                                              spaces='drive',
+                                              fields='nextPageToken, files(id, name)').execute()
+        for file in response.get('files', []):
+            try:
+                drive_service.files().delete(fileId=file.get('id')).execute()
+            except HttpError as error:
+                print(f'An error occurred: {error}')
